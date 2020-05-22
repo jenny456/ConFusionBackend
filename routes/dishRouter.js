9eb756d8bebff5ada6d9419d3041257a3f7782c8
jenny456 <dsouzajenisha82@gmail.com>
2020-05-22 14:08:32 +0530

    Added comment rights

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -183,7 +183,7 @@ dishRouter.route('/:dishId/comments/:commentId')
     Dishes.findById(req.params.dishId)
 
     .then((dish) => {
-        if (dish != null && dish.comments.id(req.params.commentId) != null) {
+        if (dish != null && dish.comments.id(req.params.commentId) != null  && dish.comments.id(req.params.commentId).author.equals(req.user._id)) {
             if (req.body.rating) {
                 dish.comments.id(req.params.commentId).rating = req.body.rating;
             }
@@ -192,13 +192,11 @@ dishRouter.route('/:dishId/comments/:commentId')
             }
             dish.save()
             .then((dish) => {
-                Dishes.findById(dish._id)
-                .populate('comments.author')
-                .then((dish) => {
+                 
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
                     res.json(dish); 
-                })                              
+                                            
             }, (err) => next(err));
         }
         else if (dish == null) {
@@ -206,10 +204,15 @@ dishRouter.route('/:dishId/comments/:commentId')
             err.status = 404;
             return next(err);
         }
-        else {
+        else if (dish.comments.id(req.params.commentId) == null) {
             err = new Error('Comment ' + req.params.commentId + ' not found');
             err.status = 404;
             return next(err);            
+        }
+        else {
+            err = new Error('you are not authorized to update this comment!');
+            err.status = 403;
+            return next(err);  
         }
     }, (err) => next(err))
     .catch((err) => next(err));
@@ -217,17 +220,15 @@ dishRouter.route('/:dishId/comments/:commentId')
 .delete(authenticate.verifyUser,(req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
-        if (dish != null && dish.comments.id(req.params.commentId) != null) {
+        if (dish != null && dish.comments.id(req.params.commentId) != null && dish.comments.id(req.params.commentId).author.equals(req.user._id)) {
             dish.comments.id(req.params.commentId).remove();
             dish.save()
             .then((dish) => {
-                Dishes.findById(dish._id)
-                .populate('comments.author')
-                .then((dish) => {
+                
                     res.statusCode = 200;
                     res.setHeader('Content-Type', 'application/json');
                     res.json(dish); 
-                })                 
+                                 
             }, (err) => next(err));
         }
         else if (dish == null) {
@@ -235,10 +236,15 @@ dishRouter.route('/:dishId/comments/:commentId')
             err.status = 404;
             return next(err);
         }
-        else {
+        else if (dish.comments.id(req.params.commentId) == null) {
             err = new Error('Comment ' + req.params.commentId + ' not found');
             err.status = 404;
             return next(err);            
+        }
+        else {
+            err = new Error('you are not authorized to delete this comment!');
+            err.status = 403;
+            return next(err);  
         }
     }, (err) => next(err))
     .catch((err) => next(err));
