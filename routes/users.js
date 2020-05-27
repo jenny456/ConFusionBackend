ddc24fea9221c7ca4bb00ab9d46c79394dbdd744
jenny456 <dsouzajenisha82@gmail.com>
2020-05-27 13:56:10 +0530

    Integrating client and server

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
+router.options('*',cors.corsWithOptions,(req,res) => {res.sendStatus(200); })
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
   User.find({})
   .then((users) => {
@@ -47,11 +48,27 @@ router.post('/signup',cors.corsWithOptions, (req, res, next) => {
   });
 });
 
-router.post('/login', cors.corsWithOptions,passport.authenticate('local'), (req, res) => {
-  var token=authenticate.getToken({_id:req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true,token: token, status: 'You are successfully logged in!'});
+router.post('/login', cors.corsWithOptions, (req, res,next) => {
+  passport.authenticate('local',(err,user,info) => {
+      if(err)
+        return next(err);
+      if(!user) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false,status: 'log in unsucessful!',err:info});
+      }
+      req.logIn(user,(err) => {
+        if(err) {
+          res.statusCode = 401;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success:false, status: 'unsuccessful log in!',err:'Could not log in user!'});
+        }
+        var token=authenticate.getToken({_id:req.user._id});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Login Successful!', token: token});
+      });
+  }) (req,res,next);
 });
 
 router.get('/logout', cors.corsWithOptions,(req, res) => {
@@ -65,5 +82,23 @@ router.get('/logout', cors.corsWithOptions,(req, res) => {
     err.status = 403;
     next(err);
   }
+});
+
+router.get('/checkJWTToken',cors.corsWithOptions,(req,res,next) => {
+  passport.authenticate('jwt',{session:false},(err,user,info) => {
+    if(err) {
+      return next(err);
+    }
+    if(!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+    }
+  })(req,res);
 });
 module.exports = router;
